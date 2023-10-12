5a9a2462f7ead06ba79bda26495f859bfa7ae5e8
valerepetto14 <valentinrepetto12345@gmail.com>
2023-10-12 14:06:08 -0300

    set morgan and fix dev dependencies

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,14 @@ import dotenv from 'dotenv';
 dotenv.config();
 import Express from 'express';
 import cors from 'cors';
+import morgan from 'morgan';
 
 import { Auth } from './routes';
 import handleErrors from './middlewares/handleErrors';
 
 const app = new App({
     port: 3001,
-    middleWares: [Express.json(), Express.urlencoded({ extended: true }), cors()],
+    middleWares: [Express.json(), Express.urlencoded({ extended: true }), cors(), morgan('dev')],
     routes: [
         ['/auth', Auth],
         ['/*', handleErrors],
